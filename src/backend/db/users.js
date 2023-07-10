3646bab14f1714e7a672af0111a17a96c01d5bc1
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-07-10 23:01:42 +0530

    added: bio & website in users db

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -13,6 +13,8 @@ export const users = [
     email: "neeyasharmaa@gmail.com",
     password: "neeya123",
     image: "/assets/images/guest_user.jpg",
+    bio: "Bookworm with an insatiable appetite for words, worlds, and the magic they hold...",
+    website: "https://example.com/neeyasharmaa",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
@@ -25,6 +27,8 @@ export const users = [
     password: "kath123",
     image:
       "https://images.unsplash.com/photo-1521417531039-75e91486cc40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHNpbmdlciUyMHdvbWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    bio: "Singing my way through life's symphony 🎵",
+    website: "https://example.com/kathwilson",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
@@ -37,6 +41,8 @@ export const users = [
     password: "noah123",
     image:
       "https://images.unsplash.com/photo-1554456854-55a089fd4cb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fGRvZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    bio: "Embracing the beauty of the natural world ",
+    website: "https://example.com/noahdavis",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
@@ -49,6 +55,8 @@ export const users = [
     password: "james123",
     image:
       "https://images.unsplash.com/photo-1531218614045-e596f12f0393?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTB8fGhhcHB5JTIwbWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    bio: "Capturing moments and telling stories through the lens 📷",
+    website: "https://example.com/jamieanderson",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
